8ecc3e918ff3a04b4627387b82a658bf7dbd5bb9
J Rafael R Nascimento <63913814+jrafaelrn@users.noreply.github.com>
2023-06-12 17:38:24 -0300

    create env and classes

---
+++ db.py
@@ -0,0 +1,26 @@
+import psycopg2
+
+
+class db:
+
+    def __init__(self):
+        self.connect_db()
+        
+
+    def connect_db(self):
+        self.conn = psycopg2.connect("dbname=postgres user=postgres password=postgres")
+        
+
+    def desconect_db(self):
+        self.conn.close()
+        
+
+    def execute_bd(self, command: str):
+        
+        cur = self.conn.cursor()
+        cur.execute(command)
+        self.conn.commit()
+        cur.close()
+        
+        if command.startswith("SELECT"):
+            return cur.fetchall()
---
+++ gui.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import tkinter as tk
+import numpy as np
+
